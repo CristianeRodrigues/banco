0ed2caf39626178580dd25e8e78ac1f846c7a430
cristiane <cris_csr@yahoo.com.br>
2019-02-12 13:11:16 -0200

    primeiro commit

---
+++ src/banco/Cliente.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author Cristiane
+ */
+public class Cliente {
+    
+    String nome;
+    int ag;
+    Conta cc;
+    
+     public String getNome() {
+        return this.nome;
+    }
+     
+     public void setAg() {
+        System.out.println("Agencia");
+    }
+    
+}
